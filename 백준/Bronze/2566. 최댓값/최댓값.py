ff0732ee5feff0fe2767f5e332ea6cd86cb97434
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2023-06-17 21:42:34 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,19 @@
+matrix = []  # 2차원 리스트
+
+for _ in range(9):  # 9줄에 대해 반복
+    # 입력받은 값을 공백을 기준으로 분리하여 정수로 변환하여 리스트에 저장
+    row = list(map(int, input().split()))
+    matrix.append(row)  # 행을 2차원 리스트에 추가
+
+max = 0
+max_x=1
+max_y=1
+for i in range(9):
+    for j in range(9):
+        if(max<matrix[i][j]):
+            max=matrix[i][j]
+            max_x=i
+            max_y=j
+
+print(max)
+print(max_x+1,max_y+1)
