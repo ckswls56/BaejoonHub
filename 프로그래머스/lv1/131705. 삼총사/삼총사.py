204e70b3d64555dca279a74675726c4758819141
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2023-09-17 22:35:52 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -3,6 +3,6 @@ def solution(number):
     answer = 0
     comb = list(combinations(number,3))
     for c in comb :
-        if c[0]+c[1]+c[2] == 0 :
-            answer+=1
+        if sum(c) == 0:
+            answer += 1
     return answer
