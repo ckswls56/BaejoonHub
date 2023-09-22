45bd85af524af45e92bfd1217cf322ccb26bd58a
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2023-09-22 17:35:16 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,9 +1,4 @@
 def solution(strings, n):
-    # 문자열을 정렬하는 사용자 정의 함수
-    def custom_sort(s):
-        return (s[n], s)
-    
-    # sorted() 함수를 사용하여 정렬
-    sorted_strings = sorted(strings, key=custom_sort)
-    
-    return sorted_strings
+    def sort(s):
+        return (s[n],s)
+    return sorted(strings,key=sort)
