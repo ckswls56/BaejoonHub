7140f01ea5fa18c047b60b7fefd57694fa821f79
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-01-09 23:57:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.47 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 2
+
+    for i in range(2, n):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n-1] % 1234567
