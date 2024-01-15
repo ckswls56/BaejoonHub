b2494c6b6f9d0e972949215e14344df6f99f5a19
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-01-16 01:15:00 +0900

    [level unrated] Title: 주사위 고르기, Time: 990.58 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/unrated/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/unrated/258709. 주사위 고르기/주사위 고르기.py
@@ -25,12 +25,16 @@ def solution(dice):
         for c in combo
     ]
     max_win = 0
-    for i in range(len(res)):
+    for i in range(len(res)//2):
         win = count_case(res[i],res[len(res)-i-1])
         if win>max_win:
             max_win=win
             ret = i
     
+    #2개인 경우 
+    if len(res)//2 == 1 and max_win < 18:
+        ret+=1
+        
     
     result = []        
     for c in combo_indices[ret] :
