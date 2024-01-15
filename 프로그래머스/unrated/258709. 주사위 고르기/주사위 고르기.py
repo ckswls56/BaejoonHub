7e706a4e1f973fd35126f73ea0703fd764036ce7
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-01-16 01:14:29 +0900

    [level unrated] Title: 주사위 고르기, Time: 990.58 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258709. 주사위 고르기/주사위 고르기.py
@@ -0,0 +1,39 @@
+from itertools import combinations, product
+
+def add_one(n):
+    return n + 1
+
+def count_case(list_a, list_b):
+    count = 0
+    index_b = 0
+    
+    for elem_a in list_a:
+        while index_b < len(list_b) and list_b[index_b] < elem_a:
+            index_b += 1
+        count += index_b
+    
+    return count
+
+def solution(dice):
+    indices=list(range(len(dice)))
+    combo_indices = list(combinations(indices, len(indices)//2))
+    
+    combo = list(combinations(dice, len(dice)//2))
+    
+    res = [
+        sorted([sum(r) for r in product(*c)])
+        for c in combo
+    ]
+    max_win = 0
+    for i in range(len(res)):
+        win = count_case(res[i],res[len(res)-i-1])
+        if win>max_win:
+            max_win=win
+            ret = i
+    
+    
+    result = []        
+    for c in combo_indices[ret] :
+        result.append(c+1)
+    
+    return result
