eac99c6fe7566e2328c0ff2354a1657197d1ae02
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-01-22 04:45:34 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -0,0 +1,40 @@
+def solution(str1, str2):
+    answer = 0
+    set1 = []
+    set2 = []
+    
+    for i in range(len(str1)-1):
+        temp = str1[i:i+2]
+        if temp.isalpha():
+            set1.append(temp.upper())
+    
+    for i in range(len(str2)-1):
+        temp = str2[i:i+2]
+        if temp.isalpha():
+            set2.append(temp.upper())
+            
+    temp_set1 = set1[:]
+    union = set1[:]
+    intersection = []
+    
+    for s2 in set2:
+        if s2 not in temp_set1:
+            union.append(s2)
+        else :
+            temp_set1.remove(s2)
+    
+    for s1 in set1:
+        if s1 in set2:
+            set2.remove(s1)
+            intersection.append(s1)
+    
+    
+    if len(union)==0 and len(intersection) == 0 :
+        return 65536
+    
+    answer = (int(len(intersection)/(len(union))*65536))        
+            
+    
+    
+            
+    return answer
