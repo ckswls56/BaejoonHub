346c347636dea52f41594260e474de8c2f010019
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-01-25 01:47:37 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 43.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,3 +1,33 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
 def solution(jobs):
 
     answer = 0
@@ -6,7 +36,7 @@ def solution(jobs):
     wait = []
     work = []
     idx = 0
-    for t in range(0, 1000000):
+    for t in range(0, 500000):
         while idx<len(jobs) and jobs[idx][0] == t:
             wait += [jobs[idx]]
             idx += 1
@@ -16,7 +46,6 @@ def solution(jobs):
             if work[0][1] == t:
                 w = work.pop()
                 answer += t - w[0]
-
 
         if not work:
             if wait:
