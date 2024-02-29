73b8e5ec4100caf34fdaee5b02da715b23d5e272
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-02-29 20:09:58 +0900

    [level 3] Title: 징검다리 건너기, Time: 199.46 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -10,13 +10,13 @@ def solution(stones, k):
 
     # 먼저 0부터 k - 2 까지 최대 힙에 인덱스와 함께 넣자.
     for i in range(k - 1):
-        heapq.heappush(queue, [-stones[i], i])
+        heapq.heappush(queue, (-stones[i], i))
 
     # k - 1부턴 하나씩 최대 힙에 넣자.
     # 최대 힙의 맨 앞의 인덱스가 i - k + 1보다 작다면 구간을 벗어난 원소
     # 구간을 벗어난 원소를 전부 pop
     for i in range(k - 1, n):
-        heapq.heappush(queue, [-stones[i], i])
+        heapq.heappush(queue, (-stones[i], i))
         while queue[0][1] < i - k + 1:
             heapq.heappop(queue)
         answer = min(answer, -queue[0][0]) # 답은 각 구간의 최댓값들의 최솟값
