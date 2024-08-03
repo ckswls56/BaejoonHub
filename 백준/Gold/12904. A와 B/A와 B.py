25e6b5c7cf0e89efba40cb56e6427de9efc077da
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-08-03 19:37:03 +0900

    [Gold V] Title: A와 B, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,18 @@
+S = list(input())
+T = list(input())
+flag = False
+
+while T:
+    if T[-1] == 'A':
+        T.pop()
+    elif T[-1] == 'B':
+        T.pop()
+        T.reverse()
+    if S == T :
+        flag = True
+        break
+
+if flag:
+    print(1)
+else:
+    print(0)
