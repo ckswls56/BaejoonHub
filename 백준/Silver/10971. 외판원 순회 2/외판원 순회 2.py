c32e486ff293ef4b29ef06f8efa0f67beedd3678
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-10-01 23:08:29 +0900

    [Silver II] Title: 외판원 순회 2, Time: 6600 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,26 +1,28 @@
-def sol(arr, y, s, start):
-    # 모든 도시를 방문한 경우 출발점으로 돌아가는 비용 추가
-    if len(arr) == n:
-        return s + cost[y][start] if cost[y][start] > 0 else float('inf')  # 출발점으로 돌아갈 수 없는 경우 처리
+
+def sol(arr,y,s):
     
-    ret = float('inf')
+    if len(arr) == n :
+        return s+ cost[y][start] if cost[y][start] > 0 else 10000000  # 출발점으로 돌아갈 수 없는 경우 처리
     
-    # 다음 방문할 도시 탐색
+    ret = 10000000
+    
     for i in range(n):
-        if i not in arr and cost[y][i] > 0:  # 방문하지 않은 도시와 경로가 존재하는지 확인
+        if i not in arr and cost[y][i]>0:
             arr.add(i)
-            ret = min(ret, sol(arr, i, s + cost[y][i], start))
+            ret = min(ret,sol(arr,i,s+cost[y][i]))
             arr.remove(i)
     
     return ret
 
+
 n = int(input())
-cost = [list(map(int, input().split())) for _ in range(n)]
 
-ans = float('inf')
-# 각 도시를 출발점으로 설정해 탐색
+cost = [list(map(int,input().split())) for _ in range(n)]
+
+
+ans = 10000000
 for i in range(n):
-    start = i
-    ans = min(ans, sol(set([i]), i, 0, start))
-
-print(ans if ans != float('inf') else -1)  # 경로가 없는 경우 처리
+    start=i
+    ans = min(ans,sol(set({i}),i,0))
+    
+print(ans)
