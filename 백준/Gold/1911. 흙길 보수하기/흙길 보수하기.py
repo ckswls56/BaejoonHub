19f0ecf68b7a71c0853d91677f1944e04cde5ec1
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-10-09 21:40:09 +0900

    [Gold V] Title: 흙길 보수하기, Time: 52 ms, Memory: 33164 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -5,19 +5,13 @@ pools = [list(map(int, input().split())) for _ in range(n)]
 # 웅덩이 좌표 오름차순 정렬
 pools.sort()
 
-# 웅덩이를 덮은 마지막 널빤지의 위치 
-cur = 0
-# 널빤지의 개수 
-cnt = 0
+# 널빤지의 개수, 웅덩이를 덮은 널빤지의 마지막 위치 
+res, s = 0, 0
 for start, end in pools:
-    
-    # 이전 웅덩이에서 덮은 널빤지가 해당 웅덩이를 덮고있는 경우 
-    if cur > start:
-        start = cur
-    # 널빤지의 개수 카운트 
-    while start < end:
-        start += L
-        cnt += 1
-    cur = start
+  s = max(start, s)
+  diff = end - s
+  count = (diff + L - 1) // L
+  res += count
+  s += count * L
 
-print(cnt)
+print(res)
