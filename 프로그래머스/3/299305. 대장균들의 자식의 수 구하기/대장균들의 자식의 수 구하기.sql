ceacee04a841336485df70539574b00e9349349a
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-10-12 17:41:02 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,6 +1,6 @@
 select id, COALESCE(a.CHILD_COUNT, 0) as CHILD_COUNT
 from ecoli_data as b
-left outer join 
+left  join 
 (select count(*) as CHILD_COUNT , parent_id
 from ecoli_data
 group by parent_id) as a on a.parent_id = b.id
