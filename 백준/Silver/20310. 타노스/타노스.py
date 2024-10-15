1d5750d043128af8745500512e82c79fe18ee44b
Nachanjin <39850159+ckswls56@users.noreply.github.com>
2024-10-15 12:57:48 +0900

    [Silver III] Title: 타노스, Time: 40 ms, Memory: 33184 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,7 +1,23 @@
-
+from bisect import bisect_left,bisect_right
 s=list(input())
-z,o = s.count('0'),s.count('1')
 
-n_s = '0' * (z//2) + '1' * (o//2)
+z,o = s.count('0')//2,s.count('1')//2
 
-print(n_s)
+remove = []
+
+while o :
+    o-=1
+    r = s.index('1')
+    s[r] = ''
+r_s = list(reversed(s))
+while z:
+    z-=1
+    r = r_s.index('0')
+    r_s[r] = ''
+    r = len(s)-1- r
+    s[r] = ''
+
+
+n_s = list(filter(lambda x : x!='',s))
+
+print("".join(n_s))
